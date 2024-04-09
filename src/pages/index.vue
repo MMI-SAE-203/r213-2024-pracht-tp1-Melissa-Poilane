ced6b7bb095530608b5b273a18132c03ea93e96c
Melissa-Poilane <melissa.poilane@edu.univ-fcomte.fr>
2024-04-09 15:36:56 +0200

    html du menu

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue';
+
+const maVar = ref(false);
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  <button @pointerdown="maVar = !maVar">Menu</button>
+
+<button
+  aria-controls="mainNav"
+  aria-expanded="true"
+  class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+>
+  menu
+</button>
+<!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
+<nav id="mainNav">
+  <ul>
+    <li><a href="#">item 1</a></li>
+    <li><a href="#">item 2</a></li>
+    <li><a href="#">item 3</a></li>
+  </ul>
+</nav>
+
+  <h1 v-show="maVar" class="text-2xl">Bonjour le monde !</h1>
 </template>
