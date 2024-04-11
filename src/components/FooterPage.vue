e6cce92dbbbb738f54d2ca13630f6e80f9df3735
Melissa-Poilane <melissa.poilane@edu.univ-fcomte.fr>
2024-04-11 23:15:59 +0200

    mise en forme footer mobile

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,14 +1,14 @@
 <template>
-  <footer class="container">
-   <div>
+  <footer class="container bg-Default-white">
+   <div class="flex py-1 px-6 items-end space-x-1 ">
     <img src="" alt="logo du site">
-    <p>Estatery</p>
+    <p class="text-xl font-bold leading-7">Estatery</p>
    </div>
 
-<div>
-   <div>
-    <h5>SELL A HOME</h5>
-    <ul>
+<div class="grid grid-cols-2 gap-8 mb-20">
+   <div >
+    <h5 class="text-sm leading-5 font-bold mb-4">SELL A HOME</h5>
+    <ul class="text-sm leading-5 font-normal text-gray-500 ">
         <li><a href="#">Request an offer</a></li>
         <li><a href="#">Pricing</a></li>
         <li><a href="#">Reviews</a></li>
@@ -16,35 +16,28 @@
     </ul>   
    </div>
 
-   <div>
-    <h5>BUY A HOME</h5>
-    <ul>
-        <li><a href="#">Buy</a></li>
-        <li><a href="#">Finance</a></li>
-    </ul>   
-   </div>
-
-   <div>
-    <h5>BUY, RENT AND SELL</h5>
-    <ul>
-        <li><a href="#">Buy & sell properties</a></li>
-        <li><a href="#">Rent home</a></li>
-        <li><a href="#">Builder trade-up</a></li>
-    </ul>   
-   </div>
-
-   <div>
-    <h5>TERMS & PRIVACY</h5>
-    <ul>
+    <div>
+    <h5 class="text-sm leading-5 font-bold mb-4">TERMS & PRIVACY</h5>
+    <ul class="text-sm leading-5 font-normal text-gray-500 ">
         <li><a href="#">Trust & Safety</a></li>
         <li><a href="#">Terms of Service</a></li>
         <li><a href="#">Privacy Policy</a></li>
     </ul>   
    </div>
 
+    <div>
+    <h5 class="text-sm leading-5 font-bold mb-4">BUY A HOME</h5>
+    <ul class="text-sm leading-5 font-normal text-gray-500 ">
+        <li><a href="#">Buy</a></li>
+        <li><a href="#">Finance</a></li>
+    </ul>   
+   </div>
+
+ 
+
    <div>
-    <h5>ABOUT</h5>
-    <ul>
+    <h5 class="text-sm leading-5 font-bold mb-4">ABOUT</h5>
+    <ul class="text-sm leading-5 font-normal text-gray-500 ">
         <li><a href="#">Company</a></li>
         <li><a href="#">How it works</a></li>
         <li><a href="#">Contact</a></li>
@@ -52,26 +45,35 @@
     </ul>   
    </div>
 
+ <div>
+    <h5 class="text-sm leading-5 font-bold mb-4">BUY, RENT AND SELL</h5>
+    <ul class="text-sm leading-5 font-normal text-gray-500 ">
+        <li><a href="#">Buy & sell properties</a></li>
+        <li><a href="#">Rent home</a></li>
+        <li><a href="#">Builder trade-up</a></li>
+    </ul>   
+   </div>
+
    <div>
-    <h5>RESOURCES</h5>
-    <ul>
+    <h5 class="text-sm leading-5 font-bold mb-4">RESOURCES</h5>
+    <ul class="text-sm leading-5 font-normal text-gray-500 ">
         <li><a href="#">Blog</a></li>
         <li><a href="#">Guides</a></li>
         <li><a href="#">FAQ</a></li>
         <li><a href="#">Help Center</a></li>
     </ul>   
    </div>
-
-   <div>
-    <p>© 2021 Estatery. All rights reserved</p>
-    <ul>
+</div>
+   <div class="border-t border-indigo-100 pt-8">
+    <p class="text-base text-gray-900 leading-6 font-normal">© 2021 Estatery. All rights reserved</p>
+    <ul class="flex gap-10 mt-6 mb-12">
         <li><a href="#"><img src="" alt="logo de facebook"></a></li>
         <li><a href="#"><img src="" alt="logo de instagram"></a></li>
         <li><a href="#"><img src="" alt="logo de twitter"></a></li>
         <li><a href="#"><img src="" alt="logo de linkedin"></a></li>
     </ul>
    </div>
-</div>
+
   </footer>
 </template>
 
