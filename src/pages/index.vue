0c7791ceea29dff2f0838c61d6d331240e4783cc
Melissa-Poilane <melissa.poilane@edu.univ-fcomte.fr>
2024-04-11 14:01:14 +0200

    Ajout de l'accordéon pour afficher les sections dans index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -59,16 +59,26 @@ const sectionsData = [
 
   <h1 class="text-2xl">Bonjour le monde !</h1>
 
-  <section v-for="">
+  <section >
     <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 1 ? 0 : 1">bouton 1 </button>
     <p v-show="sectionOpen === 1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores hic, pariatur sed veritatis tempore atque ducimus natus sapiente eveniet, corporis laborum ullam eaque dignissimos at iste dolore consequuntur? Blanditiis, labore.</p>
   </section>
-  <section v-for="">
+  <section >
     <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 2 ? 0 : 2">bouton 2</button>
     <p v-show="sectionOpen === 2">Error quae itaque iste molestiae ipsum earum voluptas similique delectus atque laborum consectetur et non consequatur ex nihil, repudiandae alias necessitatibus nemo nisi? Reiciendis, modi quia ipsum ratione dolorem ut!</p>
   </section>
-  <section v-for="">
+  <section >
     <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 3 ? 0 : 3">bouton 3</button>
     <p v-show="sectionOpen === 3">Fuga, ea adipisci omnis dolore autem veniam reiciendis voluptate praesentium repellat aperiam ipsa, tempore voluptatibus? Ab, ea est! Eum voluptates at deserunt error accusamus omnis cum voluptatem incidunt dicta dolore.</p>
   </section>
+
+  <div v-for="(section, key) in sectionsData" :key="key">
+  <button class="accordion" @click="toggleAccordion(key)">
+    {{ section.label }}
+  </button>
+  <div class="panel" v-show="activeAccordion === key">
+    <p>{{ section.texte }}</p>
+  </div>
+</div>
+
 </template>
