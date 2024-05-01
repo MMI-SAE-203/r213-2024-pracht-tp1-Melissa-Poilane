5bfc09aa1dee6faca4fa19183ea63714a968c910
Melissa-Poilane <melissa.poilane@edu.univ-fcomte.fr>
2024-05-02 01:12:56 +0200

    Mise à jour du style du footer mobile et ajout de la transition du menu et d'un pointerdown.

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,89 +1,87 @@
+<script setup lang="ts">
+import IconLogo from './icons/IconLogo.vue'
+import IconFacebook from './icons/IconFacebook.vue'
+import IconInstagram from './icons/IconInstagram.vue'
+import IconTwitter from './icons/IconTwitter.vue'
+import IconLinkedin from './icons/IconLinkedin.vue'
+</script>
+
 <template>
-  <footer class="container bg-Default-white">
-   <div class="flex py-1 px-6 items-end space-x-1 ">
-    <img src="" alt="logo du site">
-    <p class="text-xl font-bold leading-7">Estatery</p>
-   </div>
-
-<div class="grid grid-cols-2 gap-8 mb-20">
-   <div >
-    <h5 class="text-sm leading-5 font-bold mb-4">SELL A HOME</h5>
-    <ul class="text-sm leading-5 font-normal text-gray-500 ">
-        <li><a href="#">Request an offer</a></li>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">Reviews</a></li>
-        <li><a href="#">Stories</a></li>
-    </ul>   
-   </div>
-
-    <div>
-    <h5 class="text-sm leading-5 font-bold mb-4">TERMS & PRIVACY</h5>
-    <ul class="text-sm leading-5 font-normal text-gray-500 ">
-        <li><a href="#">Trust & Safety</a></li>
-        <li><a href="#">Terms of Service</a></li>
-        <li><a href="#">Privacy Policy</a></li>
-    </ul>   
-   </div>
-
-    <div>
-    <h5 class="text-sm leading-5 font-bold mb-4">BUY A HOME</h5>
-    <ul class="text-sm leading-5 font-normal text-gray-500 ">
-        <li><a href="#">Buy</a></li>
-        <li><a href="#">Finance</a></li>
-    </ul>   
-   </div>
-
+  <footer class="container bg-Default-white  ">
+    <div class="lg:flex lg:justify-center ">
+    <div class="my-12 lg:mt-0">
+    <IconLogo /></div>
+<div class="columns-2 lg:columns-3 mb-20 space-y-8">
  
-
-   <div>
-    <h5 class="text-sm leading-5 font-bold mb-4">ABOUT</h5>
-    <ul class="text-sm leading-5 font-normal text-gray-500 ">
-        <li><a href="#">Company</a></li>
-        <li><a href="#">How it works</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Investors</a></li>
+         <div>
+    <h5>SELL A HOME</h5>
+    <ul>
+        <li><RouterLink to="#">Request an offer</RouterLink></li>
+        <li><RouterLink to="#">Pricing</RouterLink></li>
+        <li><RouterLink to="#">Reviews</RouterLink></li>
+        <li><RouterLink to="#">Stories</RouterLink></li>
+    </ul>  
+   </div>
+<div>
+    <h5>BUY A HOME</h5>
+    <ul >
+        <li><RouterLink to="#">Buy</RouterLink></li>
+        <li><RouterLink to="#">Finance</RouterLink></li>
     </ul>   
    </div>
 
- <div>
-    <h5 class="text-sm leading-5 font-bold mb-4">BUY, RENT AND SELL</h5>
-    <ul class="text-sm leading-5 font-normal text-gray-500 ">
-        <li><a href="#">Buy & sell properties</a></li>
-        <li><a href="#">Rent home</a></li>
-        <li><a href="#">Builder trade-up</a></li>
+    <div>
+    <h5>BUY, RENT AND SELL</h5>
+    <ul>
+        <li><RouterLink to="#">Buy & sell properties</RouterLink></li>
+        <li><RouterLink to="#">Rent home</RouterLink></li>
+        <li><RouterLink to="#">Builder trade-up</RouterLink></li>
+    </ul>   
+   </div>
+    
+
+   
+    <div class="break-inside-avoid-column">
+    <h5>TERMS & PRIVACY</h5>
+    <ul >
+        <li><RouterLink to="#">Trust & Safety</RouterLink></li>
+        <li><RouterLink to="#">Terms of Service</RouterLink></li>
+        <li><RouterLink to="#">Privacy Policy</RouterLink></li>
+    </ul>   
+   </div>
+   
+   <div>
+    <h5>ABOUT</h5>
+    <ul>
+        <li><RouterLink to="#">Company</RouterLink></li>
+        <li><RouterLink to="#">How it works</RouterLink></li>
+        <li><RouterLink to="#">Contact</RouterLink></li>
+        <li><RouterLink to="#">Investors</RouterLink></li>
     </ul>   
    </div>
 
+
+
    <div>
-    <h5 class="text-sm leading-5 font-bold mb-4">RESOURCES</h5>
-    <ul class="text-sm leading-5 font-normal text-gray-500 ">
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Guides</a></li>
-        <li><a href="#">FAQ</a></li>
-        <li><a href="#">Help Center</a></li>
+    <h5>RESOURCES</h5>
+    <ul>
+        <li><RouterLink to="#">Blog</RouterLink></li>
+        <li><RouterLink to="#">Guides</RouterLink></li>
+        <li><RouterLink to="#">FAQ</RouterLink></li>
+        <li><RouterLink to="#">Help Center</RouterLink></li>
     </ul>   
    </div>
 </div>
-   <div class="border-t border-indigo-100 pt-8">
+</div>
+   <div class="lg:mx-36 flex flex-col lg:flex-row justify-center items-center lg:justify-between  lg:items-baseline border-t-2 border-indigo-100 pt-8 ">
     <p class="text-base text-gray-900 leading-6 font-normal">© 2021 Estatery. All rights reserved</p>
     <ul class="flex gap-10 mt-6 mb-12">
-        <li><a href="#"><img src="" alt="logo de facebook"></a></li>
-        <li><a href="#"><img src="" alt="logo de instagram"></a></li>
-        <li><a href="#"><img src="" alt="logo de twitter"></a></li>
-        <li><a href="#"><img src="" alt="logo de linkedin"></a></li>
+        <li><a href="#"><IconFacebook/></a></li>
+        <li><a href="#"><IconInstagram/></a></li>
+        <li><a href="#"><IconTwitter/></a></li>
+        <li><a href="#"><IconLinkedin/></a></li>
     </ul>
    </div>
 
   </footer>
 </template>
-
-<script>
-export default {
-  name: 'FooterPage',
-  // Vous pouvez ajouter des propriétés, des méthodes, etc. ici
-}
-</script>
-
-<style scoped>
-/* Ajoutez vos styles de pied de page ici */
-</style>
